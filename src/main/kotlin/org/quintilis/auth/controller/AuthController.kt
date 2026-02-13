eddb4ec24f1eed215481b2a69a6c5e20dcb4b775
rafinha285 <rafael.schrott02@gmail.com>
2026-02-13 08:08:47 -0300

    auth controller added

---
+++ src/main/kotlin/org/quintilis/auth/controller/AuthController.kt
@@ -0,0 +1,14 @@
+package org.quintilis.auth.controller
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/auth")
+class AuthController {
+    @PostMapping("/login")
+    fun login(){
+
+    }
+}
