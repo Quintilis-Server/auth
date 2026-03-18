3822d41f9b3d05b7b07bb96a3ba03da583773abf
abacate <rafael.schrott02@gmail.com>
2026-03-18 17:49:37 -0300

    COnsts added

---
+++ src/main/frontend/src/Consts.ts
@@ -0,0 +1,13 @@
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+
+export const AUTH_URL = import.meta.env.VITE_AUTH_URL || "http://localhost:9000";
+
+export const FORUM_URL = import.meta.env.VITE_FORUM_URL || "http://localhost:3000";
+
+export const MAP_URL = import.meta.env.VITE_MAP_URL || "http://localhost:3001";
+
+export const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL || "https://quintilis.org"
+
+export const API_AUTH_ROUTES = `${AUTH_URL}/auth`; // Direto no Auth Service (ex: /auth/register)
+export const API_OAUTH2_ROUTES = `${AUTH_URL}/oauth2`; // Endpoints OAuth2 (ex: /oauth2/token)
+export const API_FORUM_ROUTES = `${API_URL}/api/forum`; // Via Gateway
